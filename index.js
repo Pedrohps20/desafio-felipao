ea86ebde9415202fd6e1b0b9ff0ecca415666bcd
pedrohps <pedropratessouza@gmail.com>
2024-07-11 01:37:47 -0300

    meu segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-asass
+let nome = "Pedro"
+let xp = 5000
+let lvl
+
+if (xp >= 0 && xp <= 1000){
+    lvl = "Ferro"
+}
+else if (xp <= 2000){
+    lvl = "Bronze"
+}
+else if (xp <= 5000){
+    lvl = "Prata"
+}
+else if (xp <= 7000){
+    lvl = "Ouro"
+}
+else if (xp <= 8000){
+    lvl = "Platina"
+}
+else if (xp <= 9000){
+    lvl = "Ascendente"
+}
+else if (xp <= 1000){
+    lvl = "Imortal"
+}
+else lvl = Radiante
+
+console.log("O heroi " + nome + " está no nível " + lvl)
